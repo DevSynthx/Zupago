ea57bceb84bb7e73d2ea2cd5f8e03e5992ec29f6
DavidEtoedia <etoedia@gmail.com>
2023-08-27 03:48:49 -0700

    Initial Commit

---
+++ Zupago/ZupagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZupagoApp.swift
+//  Zupago
+//
+//  Created by Inyene Etoedia on 27/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZupagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
